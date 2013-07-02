007e06d0b63bd040477eda38d037d64dcd1b86b3
Adam Lavin <adam@lavoaster.co.uk>
2013-07-02 18:54:57 +0100

    Initial Commit

---
+++ src/net/battlenexus/paintball/Paintball.java
@@ -0,0 +1,12 @@
+package net.battlenexus.paintball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Paintball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //TODO Write onEnable method
+    }
+
+}
