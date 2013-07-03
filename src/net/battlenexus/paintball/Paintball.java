bda4599f3e369feebfcd626dcfe1ca75e0b07f73
Adam Lavin <adam@lavoaster.co.uk>
2013-07-03 20:48:21 +0100

    Added ScoreManager for managing the scoreboard

--- src/net/battlenexus/paintball/Paintball.java
+++ src/net/battlenexus/paintball/Paintball.java
@@ -1,12 +1,14 @@
 package net.battlenexus.paintball;
 
+import net.battlenexus.paintball.scoreboard.ScoreManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Paintball extends JavaPlugin {
+    ScoreManager scoreboard;
 
     @Override
     public void onEnable() {
-        //TODO Write onEnable method
+        scoreboard = new ScoreManager();
+        //TODO Code
     }
-
 }
