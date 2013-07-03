d65955fc55942655d4ad405815f3d0ab34641bbb
Adam Lavin <adam@lavoaster.co.uk>
2013-07-03 23:52:25 +0100

    Added getPlayer and newPlayer to PBPlayer class

--- src/net/battlenexus/paintball/entities/PBPlayer.java
+++ src/net/battlenexus/paintball/entities/PBPlayer.java
@@ -1,5 +1,36 @@
 package net.battlenexus.paintball.entities;
 
-public class PBPlayer {
+import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
+public class PBPlayer {
+    private static HashMap<String, PBPlayer> players = new HashMap<String, PBPlayer>();
+    private Player player;
+
+    private PBPlayer(Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Creates a new {@link PBPlayer} for a player
+     *
+     * @param player Player the player you would like to find
+     * @return PBPlayer
+     */
+    public static PBPlayer newPlayer(Player player) {
+        PBPlayer pbPlayer = new PBPlayer(player);
+        players.put(player.getName(), pbPlayer);
+
+        return pbPlayer;
+    }
+
+    /**
+     * Finds and returns the {@link PBPlayer} for the given player
+     * @param player Player The player you would like to find
+     * @return PBPlayer
+     */
+    public static PBPlayer getPlayer(Player player) {
+        return PBPlayer.players.get(player.getName());
+    }
 }
