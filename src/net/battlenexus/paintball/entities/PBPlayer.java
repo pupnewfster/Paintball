521f4ad1a0e4363e217e7112ea77a62ec0e92306
Adam Lavin <adam@lavoaster.co.uk>
2013-07-03 23:25:20 +0100

    Removed bodhi's IDE Comment >_>

--- src/net/battlenexus/paintball/entities/PBPlayer.java
+++ src/net/battlenexus/paintball/entities/PBPlayer.java
@@ -1,10 +1,5 @@
 package net.battlenexus.paintball.entities;
 
-/**
- * Code written by AutumnDusk (Bodhi Mulders)
- * Visit http://www.autumndusk.net/ for more information
- */
-
 public class PBPlayer {
 
 }
