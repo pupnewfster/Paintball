fe7274bd4e040e487bcf4f5a23da22fdaacb6ce3
Bodhi Mulders <bodhimulders@autumndusk.net>
2013-07-03 23:18:47 +0200

    Ignored netbeans config folder

---
+++ src/net/battlenexus/paintball/entities/PBPlayer.java
@@ -0,0 +1,10 @@
+package net.battlenexus.paintball.entities;
+
+/**
+ * Code written by AutumnDusk (Bodhi Mulders)
+ * Visit http://www.autumndusk.net/ for more information
+ */
+
+public class PBPlayer {
+
+}
