37173e5119c4ecee0a326206dba9af0e85f5c05a
Adam Lavin <adam@lavoaster.co.uk>
2013-09-12 16:04:09 +0100

    Add players to PBPlayer if server players already exist on plugin load.

--- src/main/java/net/battlenexus/paintball/Paintball.java
+++ src/main/java/net/battlenexus/paintball/Paintball.java
@@ -1,8 +1,10 @@
 package net.battlenexus.paintball;
 
+import net.battlenexus.paintball.entities.PBPlayer;
 import net.battlenexus.paintball.listeners.PlayerListener;
 import net.battlenexus.paintball.listeners.TickBukkitTask;
 import net.battlenexus.paintball.scoreboard.ScoreManager;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Paintball extends JavaPlugin {
@@ -20,6 +22,10 @@ public class Paintball extends JavaPlugin {
 
         tasks = new TickBukkitTask();
         tasks.runTaskTimerAsynchronously(this, 1, 1);
+
+        for(Player player : getServer().getOnlinePlayers()) {
+            PBPlayer.newPlayer(player);
+        }
     }
 
     private void registerListeners() {
