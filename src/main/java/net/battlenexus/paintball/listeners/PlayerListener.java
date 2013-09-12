2461a02099393ad12e165d226e3801e7bd298b25
Adam Lavin <adam@lavoaster.co.uk>
2013-09-12 16:16:49 +0100

    Now kills player instantly when hit with a projectile...

--- src/main/java/net/battlenexus/paintball/listeners/PlayerListener.java
+++ src/main/java/net/battlenexus/paintball/listeners/PlayerListener.java
@@ -1,16 +1,23 @@
 package net.battlenexus.paintball.listeners;
 
 import net.battlenexus.paintball.entities.PBPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public class PlayerListener implements Listener {
+
+    protected HashMap<String, String> deathMessages = new HashMap<>();
 
     public PlayerListener(JavaPlugin plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -18,15 +25,30 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
+        PBPlayer.newPlayer(event.getPlayer());
     }
 
     @EventHandler
     public void onPlayerHit(EntityDamageByEntityEvent event) {
-        if(!(event.getDamager() instanceof Player)) {
+        if(!(event.getDamager() instanceof Projectile) || !(event.getEntity() instanceof Player) || !(((Projectile) event.getDamager()).getShooter() instanceof Player)) {
             return;
         }
-        //TODO
+
+        PBPlayer shooter = PBPlayer.getPlayer((Player) ((Projectile) event.getDamager()).getShooter());
+        PBPlayer target  = PBPlayer.getPlayer((Player) event.getEntity());
+
+        deathMessages.put(target.getBukkitPlayer().getName(), shooter.getBukkitPlayer().getDisplayName() + " has killed " + target.getBukkitPlayer().getDisplayName() + ". :(");
+        target.kill(shooter);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        String playerName = event.getEntity().getName();
+
+        if (deathMessages.containsKey(playerName)) {
+            event.setDeathMessage(deathMessages.get(playerName));
+            deathMessages.remove(playerName);
+        }
     }
 
     @EventHandler
