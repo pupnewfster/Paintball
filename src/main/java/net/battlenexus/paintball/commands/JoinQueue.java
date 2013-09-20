231fe18babcbae83323859a70e64fd180b2e06c8
pupnewfster <pupnewfster@gmail.com>
2013-09-19 23:47:39 -0400

    Added ability to use command blocks to join queue for players

--- src/main/java/net/battlenexus/paintball/commands/JoinQueue.java
+++ src/main/java/net/battlenexus/paintball/commands/JoinQueue.java
@@ -2,7 +2,9 @@ package net.battlenexus.paintball.commands;
 
 import net.battlenexus.paintball.Paintball;
 import net.battlenexus.paintball.entities.PBPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class JoinQueue implements PBCommand {
     @Override
@@ -17,7 +19,15 @@ public class JoinQueue implements PBCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        sender.sendMessage("You must be in-game to use this command!");
+        if(args.length != 1) {
+            sender.sendMessage("You must be in-game to use this command!");
+            return;
+        }
+        //Preforms the command if commandblock has /join @p so that the player just steps on pressure plate
+        Player target = Bukkit.getPlayer(args[0]);
+        if(target != null) {
+            target.performCommand("join");
+        }
     }
 
     @Override
