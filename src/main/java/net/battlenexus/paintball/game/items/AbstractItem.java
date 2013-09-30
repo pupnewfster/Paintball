2d5a1df6b76fe5456b9355ca2658d5e467b89c42
pupnewfster <pupnewfster@gmail.com>
2013-09-30 12:01:17 -0400

    Added some items for power up type things

---
+++ src/main/java/net/battlenexus/paintball/game/items/AbstractItem.java
@@ -0,0 +1,40 @@
+package net.battlenexus.paintball.game.items;
+
+import net.battlenexus.paintball.game.items.impl.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import net.battlenexus.paintball.entities.PBPlayer;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractItem {
+    public abstract Material getMaterial();
+    public abstract void addEffect(PBPlayer p, ItemStack is);
+
+    protected AbstractItem() {
+    }
+
+    //For when we add item to their inventory so we get ItemStack
+    public static ItemStack createItem(Material material, int duration, int amplifier) {
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta im = itemStack.getItemMeta();
+        List<String> lore = new ArrayList<String>();
+        String dur = Integer.toString(duration / 60) + ":" + Integer.toString(duration % 60);
+        lore.add("Duration: " + dur);
+        lore.add("Amplifier: " + amplifier);
+        im.setLore(lore);
+        itemStack.setItemMeta(im);
+        return itemStack;
+    }
+
+    public static AbstractItem getItem(Material mat) {
+        if((new Speed()).getMaterial().equals(mat))
+            return new Speed();
+        else if((new Health()).getMaterial().equals(mat))
+            return new Health();
+        return null;
+    }
+}
---
+++ src/main/java/net/battlenexus/paintball/game/items/impl/Health.java
@@ -0,0 +1,32 @@
+package net.battlenexus.paintball.game.items.impl;
+
+import net.battlenexus.paintball.entities.PBPlayer;
+import net.battlenexus.paintball.game.items.AbstractItem;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.List;
+
+public class Health extends AbstractItem {
+
+    @Override
+    public Material getMaterial() {
+        return Material.EMERALD;
+    }
+
+    //Options for amplifier should be more than 0
+    @Override
+    public void addEffect(PBPlayer p, ItemStack is) {
+        ItemMeta im = is.getItemMeta();
+        if(!is.getItemMeta().hasLore()) {
+            return;
+        }
+        p.getBukkitPlayer().getInventory().remove(is);
+        List<String> lore = im.getLore();
+        int amount = Integer.parseInt(lore.get(1).split(" ")[1]);
+        p.increasMaxHealth(amount);
+    }
+}
